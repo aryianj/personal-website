bcd7b5996e73fe6799eaaedd83fb4782053c4a7b
aryianj <acjones10@usfca.edu>
2025-03-21 04:35:00 -0700

    ugh

--- web/src/Nav.jsx
+++ web/src/Nav.jsx
@@ -1,25 +1,26 @@
 import { MdOutlineStar } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function Nav() {
 
   return (
     <>
-   
           <nav className='absolute w-screen top-15 mx-30'>
             <ul className="flex flex-row font-medium gap-10 rounded-lg w-fit">
               <li>
-                <a href="/works" className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100">WORKS</a>
+              <Link to="/works"><p className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100">WORKS</p></Link>
               </li>
               <li>
-                <a href="/about" className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100">ABOUT</a>
+              <Link to="/about"><p className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100">ABOUT</p></Link>
               </li>
               <li>
-                <a href="/contact" className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100 ">CONTACT</a>
+              <Link to="/contact"><p className="block py-2 px-3 text-gray-900 rounded-sm text-4xl hover:bg-gray-100 ">CONTACT</p></Link>
               </li>
               <li>
-                <a href='/'>
+              <Link to="/"><p>
                   <h1 className='text-center text-7xl '>Aryian Jones</h1>
-                </a>
+                </p>
+                </Link>
               </li>
               <li> 
                 <MdOutlineStar className="text-9xl rotate-12 -mt-8 text-[#1480CD]" />
